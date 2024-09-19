54f52374fab00020adff61d04d585c3282ac765e
Martin <martin.jakobsen@cheffelo.com>
2024-09-19 18:01:37 +0200

    Fix the shit

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -3,13 +3,9 @@ import { Rating } from "@/components/Rating";
 
 export default function HomePage() {
   return (
-    <main className="p-8">
+    <main className="flex flex-col gap-6 max-w-screen-sm mx-auto py-20">
       <DragDropForm />
-<<<<<<< HEAD
       <Rating />
-    </>
-=======
     </main>
->>>>>>> f6caf7c (what)
   );
 }
