3586f3f3b8e9fb5402ede005b3120fbe599dcfa5
Johnny <xohnny1337@gmail.com>
2024-09-19 17:59:31 +0200

    what

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -3,9 +3,13 @@ import { Rating } from "@/components/Rating";
 
 export default function HomePage() {
   return (
-    <>
+    <main className="p-8">
       <DragDropForm />
+<<<<<<< HEAD
       <Rating />
     </>
+=======
+    </main>
+>>>>>>> f6caf7c (what)
   );
 }
