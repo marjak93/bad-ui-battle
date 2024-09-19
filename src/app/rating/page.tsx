94b2d0cf18a7397b102863abe6bf260e586f321f
Martin <martin.jakobsen@cheffelo.com>
2024-09-19 17:51:05 +0200

    ship it

---
+++ src/app/rating/page.tsx
@@ -0,0 +1,11 @@
+import { Rating } from "@/components/Rating";
+
+const RatingPage = () => {
+  return (
+    <div className="max-w-screen-md mx-auto py-10">
+      <Rating />
+    </div>
+  );
+};
+
+export default RatingPage;
